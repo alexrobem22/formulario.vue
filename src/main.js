5ec42907c5f5422e25cf3593e5ce6187af532afd
alexrobem22 <alex17_sv@hotmail.com>
2023-02-09 11:43:15 -0300

    subindo projeto de formulario

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { vMaska } from "maska"
+import moment from 'moment'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.directive( "maska", vMaska) // directive ou use aqui e distribuida para viu.js de forma de pugin
+app.config.globalProperties.$moment = moment // aqui e propriedade global foi criado de javascript de forma generica ai passo com propriedade
+app.config.globalProperties.$moment.locale('pt-br')
+app.mount('#app')
